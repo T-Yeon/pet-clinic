39bce23f8d6ac47fd968f0e884a7250385bee7e0
T-Yeon <thompson_yeon@bah.com>
2022-12-06 10:58:53 -0600

    initial commit

---
+++ src/test/java/thompson/yeon/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package thompson.yeon.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
